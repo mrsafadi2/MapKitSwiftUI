c239ef58f0d18bb68b400247693c90b4bff905e8
mrsafadi2 <mrsafadi2@gmail.com>
2023-01-02 16:10:43 +0200

    Initial Commit

---
+++ MapKitSwiftUI/MapKitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitSwiftUIApp.swift
+//  MapKitSwiftUI
+//
+//  Created by Mohammed Safadi Macbook Pro on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapKitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
